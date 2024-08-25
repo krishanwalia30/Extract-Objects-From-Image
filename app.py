03abe47454046a39d41a1a8390ab0f2be80c38f9
Krishan Walia <krishanw30@gmail.com>
2024-08-25 21:48:23 +0530

    Fixes

--- app.py
+++ app.py
@@ -1,13 +1,32 @@
 import streamlit as st
-import detectObjects
 from segment_anything import sam_model_registry, SamAutomaticMaskGenerator, SamPredictor
-from IPython.display import display, Image
+# from IPython.display import display, Image
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 from rembg import remove
 from PIL import Image
 
+
+# Content of detectObjects.py file 
+# import detectObjects
+import ultralytics
+from ultralytics import YOLO
+
+model = YOLO('yolov8n.pt')
+
+def detected_objects(filename:str):
+    results = model.predict(source=filename, conf=0.25)
+
+    categories = results[0].names
+
+    dc = []
+    for i in range(len(results[0])):
+        cat = results[0].boxes[i].cls
+        dc.append(categories[int(cat)])
+
+    print(dc)
+    return results, dc
 
 def show_mask(mask, ax, random_color=False):
     if random_color:
